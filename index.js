f460f7729dd9015b4da5e1cccd3e37010778bcf3
Gabe Levasseur <gabel0287@gmail.com>
2018-11-06 10:13:17 -0800

    Add error handling for no config file

--- index.js
+++ index.js
@@ -32,8 +32,12 @@ const config = convict({
     },
 });
 
-config.loadFile(`${getRootLevel()}/.ghpr.json`);
-config.validate({ allowed: 'strict' });
+try {
+    config.loadFile(`${getRootLevel()}/.ghpr.json`);
+    config.validate({ allowed: 'strict' });
+} catch(e) {
+    debuglog('Could not load config file.')
+}
 
 const prompts = {
     title: {
@@ -175,34 +179,44 @@ function getRootLevel() {
 }
 
 async function renderBody() {
-    let body = fs.readFileSync(`${getRootLevel()}/.github/pull_request_template.md`, 'utf8');
+    try {
+        let body = fs.readFileSync(`${getRootLevel()}/.github/pull_request_template.md`, 'utf8');
 
-    const userInputs = config.get('templateVariables.userInputs');
-    const commands = config.get('templateVariables.commands');
-    return Promise.each(Object.keys(userInputs), key => {
-        return inquirer
-            .prompt({
-                type: 'input',
-                name: key,
-                default: userInputs[key].default || '',
-                message: userInputs[key].message,
-                validate: function(text) {
-                    if (text.length < 0) {
-                        return `Value cannot be empty`;
-                    }
-                    return true;
-                },
-            })
-            .then(r => {
-                body = body.replace(`{{${key}}}`, r[key]);
-            });
-    })
-        .then(() => {
-            return Promise.each(Object.keys(commands), c => {
-                body = body.replace(`{{${c}}}`, shell.exec(commands[c]).stdout.trim());
-            });
+        const userInputs = config.get('templateVariables.userInputs');
+        const commands = config.get('templateVariables.commands');
+        return Promise.each(Object.keys(userInputs), key => {
+            return inquirer
+                .prompt({
+                    type: 'input',
+                    name: key,
+                    default: userInputs[key].default || '',
+                    message: userInputs[key].message,
+                    validate: function(text) {
+                        if (text.length < 0) {
+                            return `Value cannot be empty`;
+                        }
+                        return true;
+                    },
+                })
+                .then(r => {
+                    body = body.replace(`{{${key}}}`, r[key]);
+                });
         })
-        .then(() => body);
+            .then(() => {
+                return Promise.each(Object.keys(commands), c => {
+                    body = body.replace(`{{${c}}}`, shell.exec(commands[c]).stdout.trim());
+                });
+            })
+            .then(() => body);
+    } catch (e) {
+        debuglog('Could not render template: ', e);
+
+        return await inquirer.prompt({
+            type: 'input',
+            name: 'body',
+            message: 'Enter Pull Request Body: ',
+        }).body
+    }
 }
 
 function getGithubRemoteInfo() {
